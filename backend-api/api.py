be4a1ac25458ed5db6a6913abb713f3dafa40175
ChihChieh Huang <soem.hcc@gmail.com>
2020-11-05 15:40:26 +0800

    ADD. add /health and /version api

--- backend-api/api.py
+++ backend-api/api.py
@@ -24,5 +24,14 @@ def user_force_update():
 def user_delete():
     return "<h1>Hello Flask!</h1>"
 
+
+@app.route('/version', methods=['GET'])
+def version():
+    return 'dev'
+
+@app.route('/health', methods=['GET'])
+def health():
+    return 'ok'
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
