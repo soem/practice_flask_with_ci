867cf17d27bcd0b5f9c7621a399f48b85a963404
ChihChieh Huang <soem.hcc@gmail.com>
2020-11-10 00:55:18 +0800

    MOD. update README.md and ci_test.sh

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # practice_flask_with_token_based_aa
 practice_flask_with_token_based_aa
+
+
+## Start User Service
+```bash
+docker-compose build
+docker-compose up -d
+```
+
+## Run test cases
+```bash
+bash ci_test.sh
+```
---
+++ ci_test.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+
+docker-compose -f docker-compose_ci.yaml up -d
+
+docker build -t test-nodejs ci_test
+docker run -it --rm --network practice_flask_with_token_based_aa_ci test-nodejs:latest
+status=$?
+
+docker-compose -f docker-compose_ci.yaml down
+exit $status
+
